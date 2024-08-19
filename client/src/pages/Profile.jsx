44c30722d9d802654406d57bd1b7888eae06c9d1
bharath <bharathjbiet10@gmail.com>
2024-08-19 21:27:09 +0530

    backend update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -129,7 +129,9 @@ export default function Profile() {
   const handleShowListings = async () => {
     try {
       setShowListingsError(false);
-      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+     
+   
       const data = await res.json();
       if (data.success === false) {
         setShowListingsError(true);
